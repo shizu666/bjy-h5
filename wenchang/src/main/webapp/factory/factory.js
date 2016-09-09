0c8aededa73e2ef5e2e6e89d2955ad2a493084f7
zhouguangyue <zhouGUANGYUE666>
2016-09-09 18:48:04 +0800

    update

--- wenchang/src/main/webapp/factory/factory.js
+++ wenchang/src/main/webapp/factory/factory.js
@@ -1,8 +1,28 @@
 angular.module('factory',[])
-    .factory('GetContent',['$http',function ($http) {
+    .factory('HttpGet',['$http',function ($http) {
 
         return {
-           
+           back:function (url) {
+               return $http.get(url);
+           }
+        }
+
+    }])
+    .factory('HttpPost',['$http',function ($http) {
+
+        return {
+            back:function (url,params) {
+                return $http.post(url,params,{'Content-Type':'application/x-www-form-urlencoded'});
+            }
+        }
+
+    }])
+    .factory('HttpJsonp',['$http',function ($http) {
+
+        return {
+            back:function (url) {
+                return $http.jsonp(url);
+            }
         }
 
     }]);
