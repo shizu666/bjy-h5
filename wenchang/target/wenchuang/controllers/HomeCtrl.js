05a415e731a8bbf8ff34ba79b3540a6025019e8b
zhouguangyue <zhouGUANGYUE666>
2016-09-10 15:18:50 +0800

    update

--- wenchang/target/wenchuang/controllers/HomeCtrl.js
+++ wenchang/target/wenchuang/controllers/HomeCtrl.js
@@ -2,7 +2,8 @@ angular.module('Home',[])
     .controller('HomeCtrl',['$scope','$state','$stateParams','$ionicLoading','$timeout','$ionicScrollDelegate',
         function ($scope,$state,$stateParams,$ionicLoading,$timeout,$ionicScrollDelegate) {
             $scope.bannerList = ["images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg"];
-            $scope.loadMoreFlag = true;
+            $scope.loadMoreFlag = true; //上拉显示更多控制
+            $scope.showToTop = false;//显示滑动顶部按钮
             $scope.recommendList = [0,1,2,3];
 
             $scope.kindList = [
@@ -24,7 +25,6 @@ angular.module('Home',[])
                     "title":"种类"},
                 { "img":"images/6.png",
                     "title":"种类"}
-
             ];
 
             $scope.$on('$ionicView.enter', function(){
@@ -32,15 +32,32 @@ angular.module('Home',[])
 
             });
 
-            /*$scope.slideHasChanged = function (index) {
-                console.log("index",index);
-            }*/
-
+            //点击头banner
             $scope.goNextBanner = function (index) {
                 console.log("index",index);
             }
 
-            
+            //内容滚动，搜索框背景变化
+            $scope.getScrollLength = function () {
+               var postion =  $ionicScrollDelegate.getScrollPosition();
+                if(postion.top>85&&postion.top<100){
+                    $('#search').addClass("home_searchChange");
+                }else if(postion.top>=100){
+                    $('#search').addClass("home_searchChange2");
+                    $scope.showToTop = true;
+                }else{
+                    $('#search').removeClass("home_searchChange");
+                    $('#search').removeClass("home_searchChange2");
+                    $scope.showToTop = false;
+                }
+            }
+
+            //返回页面头部
+            $scope.returnTop = function () {
+                $ionicScrollDelegate.scrollTop();
+                $scope.showToTop = false;
+            }
+
             //上拉加载更多
             var a = 4;
             $scope.loadMore = function () {
@@ -48,17 +65,6 @@ angular.module('Home',[])
                 a++;
                 $scope.$broadcast('scroll.infiniteScrollComplete');
             }
-
-            //内容滚动，搜索框背景变化
-            $scope.getScrollLength = function () {
-               var postion =  $ionicScrollDelegate.getScrollPosition();
-                if(postion.top>200){
-                    $('#search').addClass("home_searchChange");
-                }else{
-                    $('#search').removeClass("home_searchChange");
-                }
-            }
-
 
 
         }]);
--- wenchang/target/wenchuang/controllers/TabCtrl.js
+++ wenchang/target/wenchuang/controllers/TabCtrl.js
@@ -1,6 +1,8 @@
 angular.module('Tab',[])
-    .controller('TabCtrl',['$scope','$state','$stateParams','$ionicLoading','$timeout','$templateCache','$http',
-        function ($scope,$state,$stateParams,$ionicLoading,$timeout,$templateCache,$http) {
+    .controller('TabCtrl',['$scope','$state','$stateParams','$ionicLoading','$timeout','$templateCache','$http','HttpJsonp',
+        'HttpGet',
+        function ($scope,$state,$stateParams,$ionicLoading,$timeout,$templateCache,$http,HttpJsonp
+        ,HttpGet) {
 
             // $ionicLoading.show({
             //     template: "正在载入数据，请稍后..."
@@ -14,24 +16,18 @@ angular.module('Tab',[])
 
             
 
-            //$scope.method = "JSONP";
-            //$scope.url = "https://angularjs.org/greet.php?callback=JSON_CALLBACK&name=Super%20Hero";
-            /*$scope.method = 'POST';
-            $scope.url = "http://10.2.0.19:8080/mall-web/adVisit";
-            var params = {
-                id:"167",
-                type:"2"
-            }
 
-            $http({method: $scope.method,data:params,url: $scope.url, cache: $templateCache}).
-            then(function(response) {
-                console.log("response",response);
-                $scope.status = response.status;
-                $scope.data = response.data;
-            }, function(response) {
-                $scope.data = response.data || 'Request failed';
-                $scope.status = response.status;
+            //$scope.url = "https://angularjs.org/greet.php?callback=JSON_CALLBACK&name=Super%20Hero";
+            //$scope.url = "http://192.168.9.109:80/mall-web/adVisit?callback=JSON_CALLBACK&id=167&type=2";
+           //$scope.url = "http://rate.tmall.com/listTagClouds.htm?itemId=44890545950&isAll=true&isInner=true&_ksTS=1440214254542_329&callback=jsonp_review_tags";
+
+            $scope.url = "http://192.168.9.109:80/mall-web/adVisit?id=167&type=2";
+           /* $http.get('http://rest.ihiss.com:9000/user/current',{
+                headers : {'X-Access-Token':token}
             });*/
 
+            HttpGet.back($scope.url).success(function (data) {
+                console.log("data",data);
+            });
 
     }]);
