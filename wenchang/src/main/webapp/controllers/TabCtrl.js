8f48c2bde8ad61ccae4aae3ded66a3aa88739bc5
zhouguangyue <zhouGUANGYUE666>
2016-09-12 16:41:57 +0800

    update

--- wenchang/src/main/webapp/controllers/TabCtrl.js
+++ wenchang/src/main/webapp/controllers/TabCtrl.js
@@ -14,27 +14,7 @@ angular.module('Tab',[])
             // },1000);
 
             
-
-            //$scope.url3 = "https://angularjs.org/greet.php?callback=JSON_CALLBACK&name=Super%20Hero";
-            $scope.url2 = "http://rate.tmall.com/listTagClouds.htm?itemId=44890545950&isAll=true&isInner=true&_ksTS=1440214254542_329&callback=JSON_CALLBACK";
-
-            $scope.url = "http://192.168.9.121:8080/mall-web/address/addressInfo?callback=JSON_CALLBACK&id=1200002";
-           // $scope.url2 = "http://192.168.9.121:8080/mall-web/address/addressInfo?id=1200002";
-           /* $http.get('http://rest.ihiss.com:9000/user/current',{
-                headers : {'X-Access-Token':token}
-            });*/
-
-
-
-           /* HttpGet.back($scope.url2).success(function (data) {
-                console.log("data2",data);
-            });*/
-
-            HttpJsonp.back($scope.url).success(function (data) {
-                console.log("data3",data);
-            }).error(function (data) {
-                console.log("cuwu",data);
-            });
+            
 
 
 
